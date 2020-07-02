ac5ff2838863032b281545b17a967bedefdcba07
choi0617 <16262606+choi0617@users.noreply.github.com>
2020-07-02 08:58:08 -0400

    completed mortage.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce = 0
+height = 100
+
+while bounce < 10:
+    newHeight = height * 0.6
+    height = newHeight
+    print(round(newHeight, 4))
+    bounce+= 1
+    
+
