b1a7829359dc5f7f3aba7ca2659fdc28a3d118ab
choi0617 <16262606+choi0617@users.noreply.github.com>
2020-07-02 12:56:15 -0400

    completed ex. 1.27

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,14 @@
 # fileparse.py
 #
 # Exercise 3.3
+import os
+print(os.getcwd())
+
+f = open('Work/Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+print(headers)
+
+for line in f:
+    row = line.split(',')
+    print(row)
+f.close()
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+total = 0
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        print('# of shares:', row[1], end='')
+        
+        p = row[2].split('\n')
+        print(' price per share:', p[0])
+
+        total += int(row[1]) * float(p[0])
+        
+print("Total:", total)       
+
+        
+        
